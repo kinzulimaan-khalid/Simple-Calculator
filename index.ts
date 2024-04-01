4e9186124bebbe6b0a29588efb3d375fd3551573
kinzulimaan-khalid <160059314+kinzulimaan-khalid@users.noreply.github.com>
2024-04-01 20:34:24 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// Printing a Welcome message
+console.log( "Welcome to kinzulimaan-khalid -CLI Simple Calculator");
+
+// Asking questions from users through inquirer
+
+const answer= await inquirer.prompt([
+    {
+    message: "Enter your first number",
+    type: "number",
+    name: "firstNumber",
+    },
+    {
+        message:"Enter your second number",
+        type: "number",
+        name: "secondNumber",
+    },
+    { message:"select one of the operator to perform operation",
+       type: "list",
+       name: "operator",
+       choices: ["Addition", "Subtraction","Multiplication","Division"] }
+]);
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+  else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+  }
+  else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer. secondNumber);
+  }
+  else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+  }
+  else { console.log("please select valid operator");
+}
+    
+
+
+
